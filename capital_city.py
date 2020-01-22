200a09cd8ca87bb70f55f8dc6c9ff441cb9a8448
mekdes <magie>
2020-01-22 16:55:59 -0600

    modified file to add class and function

--- capital_city.py
+++ capital_city.py
@@ -1,70 +1,32 @@
 # this game program displayes the country name the user guess  the capital city name.
+from game import Game # importing the file that contain the Game class.
+#creating question array.
+question_games = [
+                   "What is the capital city of Greece?:\n ",
+                   "What is the capital city of Spain?:\n",
+                   "What is the capital city of Russia?:\n"
+                 ] 
+#creating answer array
+ans_questions = [
+                  Game(question_games[0], "Athens"),
+                  Game(question_games[1], "Madrid"),
+                  Game(question_games[2], "Moscow"),
+                ]
 
-question = 0 #creating global variable
-score = 0
+def run_game(ans_questions): # define the ans the answer array.
+    score = 0  # score variable created to contain scores
+    for question in ans_questions: # for loop to loop through answers array
+        answer = input(question.quest) # answer declair to take user input.
+        if answer.lower() == question.ans.lower():# if the true is met, it adds score take andy case
+            score += 1 # add score
+    print("You answerd " + str(score))
 
-#Q1
-question1 = input("What is the capital city of Greece? \nYour Answer: ") #user answer input
-if question1 == "Athens": #if condition is meet to true count the score.
-    score += 1 # for correct question add 1 score
-    print("Correct ! ")
-    print("Scored: ", score) # print the acumilated score.
-    print("\n") #for readablity, add space on the buttom line.
+run_game(ans_questions)
 
-else:
-    print("The answer is Athens.")
-    print("You scored: ", score)
-    print("\n")
-#Q2
-question2 = input("What is the capital city of Spain? \nYour Answer: ")
-if question2 == "Madrid": 
-    score += 1 
-    print("Correct ! ")
-    print("Scored: ", score) 
-    print("\n") 
 
-else:
-    print("The correct answer is Madrid.")
-    print("You scored: ", score)
-    print("\n")
-#Q3
-question3 = input("What is the capital city of Russia? \nYour Answer: ")
-if question3 == "Moscow": 
-    score += 1 
-    print("Correct ! ")
-    print("Scored: ", score) 
-    print("\n") 
+ 
 
-else:
-    print("The correct answer is Moscow.")
-    print("You scored: ", score)
-    print("\n")
-#Q4
-question4 = input("What is the capital city of Egypt? \nYour Answer: ")
-if question4 == "Cairo":
-    score += 1 
-    print("Correct ! ")
-    print("Scored: ", score) 
-    print("\n") 
 
-else:
-    print("The answer is Cairo.")
-    print("You scored: ", score)
-    print("\n")
 
-#Qestion5
-question5 = input("what is the capital of Italy \nYour Answer: ")
-if question5 == "Rome":
-    score += 1
-    print("Correct! ")
-    print("You scored: ", score)
-    print("\n")
-else:
-    print("The correct answer is Rome. ")
-    print("You scored: ", score)
-    print("\n")
-if score >= 0:
-    print("Your total score is:", score)# it displayed the total score of the game if the total score is grater than 0.
-    print("\n")
-# else:
-#     print("Ttry again")
+
+
---
+++ game.py
@@ -0,0 +1,4 @@
+class Game:
+    def __init__(self, quest, ans):
+        self.quest = quest
+        self.ans = ans
